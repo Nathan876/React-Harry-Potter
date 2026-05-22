1f4091397abfee49c64ece260424f0c448753a1d
Nathan876 <sequins.trancheur0c@icloud.com>
2026-05-22 20:34:44 +0200

    Feat: Add theme select on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,36 @@
 import { Link } from 'react-router'
+import type {Theme} from "../contexts/ThemeContext.tsx";
+import { useTheme } from "../hooks/useTheme.tsx";
 
 export default function Header () {
-  const links = [
-    { label: 'Characters', href: '/characters' }
-  ]
+    const { theme, setTheme } = useTheme()
+    const links = [
+        { label: 'Characters', href: '/characters' }
+    ]
 
-  return (
-    <header className="flex justify-center gap-6  p-4 shadow-lg">
-      <div>
-        {links.map((link) => (
-          <Link key={link.href} to={link.href}>{link.label}</Link>
-        ))}
-      </div>
-    </header>
-  )
+    return (
+        <header className="flex justify-between items-center p-4 shadow-lg bg-black/20">
+            <div className="flex gap-6">
+                {links.map((link) => (
+                    <Link key={link.href} to={link.href} className="font-cinzel font-bold hover:opacity-80">
+                        {link.label}
+                    </Link>
+                ))}
+            </div>
+
+            <div>
+                <select
+                    value={theme}
+                    onChange={(e) => setTheme(e.target.value as Theme)}
+                    className="p-2 rounded border bg-white/10 text-inherit font-lora"
+                >
+                    <option value="Gryffindor">Gryffindor</option>
+                    <option value="Hufflepuff">Hufflepuff</option>
+                    <option value="Ravenclaw">Ravenclaw</option>
+                    <option value="Slytherin">Slytherin</option>
+                    <option value="Accessible">Accessible</option>
+                </select>
+            </div>
+        </header>
+    )
 }
