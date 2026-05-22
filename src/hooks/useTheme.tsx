d4813f0c4ef9872e5635cb401c70f05e719c4643
Nathan876 <sequins.trancheur0c@icloud.com>
2026-05-22 20:54:03 +0200

    Feat : Add theme

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,10 @@
+import {useContext} from "react";
+import {ThemeContext} from "../contexts/ThemeContext.tsx";
+
+export function useTheme() {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('Le useTheme doit être utilisé dans un ThemeProvider');
+    }
+    return context;
+}
---
+++ src/interfaces/ThemeContextType.tsx
@@ -0,0 +1,6 @@
+import type {Theme} from "../contexts/ThemeContext.tsx";
+
+export default interface ThemeContextType {
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
+}
