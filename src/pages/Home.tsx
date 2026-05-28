54e18c8e0e30c66336c3cf14375adf1784f4b030
Nathan876 <sequins.trancheur0c@icloud.com>
2026-05-28 20:36:47 +0200

    Feat : Add victory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,13 @@ import Comparator from '../components/Comparator.tsx'
 export function Home () {
   const { dailyCharacter, isLoading, error } = useDailyCharacter()
   const [lastCharacter, setLastCharacter] = useState<Character | null>(null)
+  const currentCharacter = dailyCharacter?.attributes as Character | undefined
+  const isVictory = Boolean(
+      lastCharacter &&
+      currentCharacter &&
+      lastCharacter.name === currentCharacter.name
+  )
+
 
   async function handleSelecteCharacter (character: Character) {
     console.log('Personnage du jour :', dailyCharacter)
@@ -29,11 +36,13 @@ export function Home () {
         )}
       </div>
 
-      <AutocompleteCharacter
-        label="Chercher un sorcier"
-        id="search"
-        onSelect={(character: Character) => handleSelecteCharacter(character)}
-      />
+        {!isVictory && (
+            <AutocompleteCharacter
+                label="Chercher un sorcier"
+                id="search"
+                onSelect={(character: Character) => handleSelecteCharacter(character)}
+            />
+        )}
 
       {lastCharacter !== null && (
         <Comparator
@@ -41,6 +50,15 @@ export function Home () {
           currentCharacter={dailyCharacter?.attributes as Character}
         />
       )}
+
+        {isVictory && (
+            <div className="my-8 p-6 bg-green-100 border-2 border-green-500 rounded-2xl text-center shadow-lg transform transition-all">
+                <h2 className="text-4xl font-bold text-green-700 mb-4">Victoire !</h2>
+                <p className="text-green-800 text-lg">
+                    Bravo ! Tu as trouvé <strong>{currentCharacter?.name}</strong>
+                </p>
+            </div>
+        )}
 
 
     </div>
