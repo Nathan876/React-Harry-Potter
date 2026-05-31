b17ed84d28b53c2d205a7ce556395cc7f441c196
cily <cilyana45@gmail.com>
2026-05-31 14:45:08 +0200

    feat add potion

---
+++ src/pages/PotionGame.tsx
@@ -0,0 +1,68 @@
+import Autocomplete from '../components/Autocomplete.tsx'
+import { useDailyPotion } from '../hooks/useDailyPotion.tsx'
+import type Potion from '../interfaces/Potion.tsx'
+import ComparatorPotion from '../components/potion/ComparatorPotion.tsx'
+import HelperPotion from '../components/potion/HelperPotionl.tsx'
+import { useLocalStorage } from '../hooks/useLocalStorage.ts'
+
+
+export function PotionGame () {
+  const { dailyPotion, isLoading, error } = useDailyPotion()
+  const [lastPotions, setLastPotions] = useLocalStorage<Potion []>('hp-potion-history', [])
+  const lastPotion = lastPotions.length > 0 ? lastPotions[lastPotions.length - 1] : null
+  const currentPotion = dailyPotion?.attributes as Potion | undefined
+
+  const isVictory = Boolean(
+    lastPotion &&
+    currentPotion &&
+    lastPotion.name === currentPotion.name
+  )
+
+  async function handleSelectePotion (potion: Potion) {
+    setLastPotions(prev => [...prev, potion])
+  }
+
+  return (
+    <div className="p-8 max-w-2xl mx-auto">
+      <div className="mb-8 p-4 bg-gray-100 dark:bg-gray-800 rounded-lg text-center">
+        <h2 className="text-xl font-bold mb-2">Potion du jour :</h2>
+        {isLoading ? (
+          <p className="text-gray-500">Recherche dans les archives magiques...</p>
+        ) : error ? (
+          <p className="text-red-500">{error}</p>
+        ) : (
+          <p className="text-green-600 font-bold text-2xl">
+          </p>
+        )}
+      </div>
+      {!isVictory && (
+        <Autocomplete
+          label="Search a potion"
+          id="search"
+          type="potion"
+          onSelect={(potion) => handleSelectePotion(potion as Potion)}
+        />
+      )}
+
+      {isVictory && (
+        <div
+          className="my-8 p-6 bg-green-100 border-2 border-green-500 rounded-2xl text-center shadow-lg transform transition-all">
+          <h2 className="text-4xl font-bold text-green-700 mb-4">Victoire !</h2>
+          <p className="text-green-800 text-lg">
+            Bravo ! Tu as trouvé <strong>{currentPotion?.name}</strong>
+          </p>
+        </div>
+      )}
+
+      <HelperPotion currentPotion={dailyPotion?.attributes as Potion}></HelperPotion>
+
+      {lastPotions?.length > 0 && lastPotions.toReversed().map((potion, index) => (
+        <ComparatorPotion key={`${potion.id}-${index}`} potion={potion} dailyPotion={dailyPotion?.attributes as Potion}></ComparatorPotion>
+      ))}
+
+
+    </div>
+  )
+}
+
+export default PotionGame
--- src/services/PotionService.tsx
+++ src/services/PotionService.tsx
@@ -2,21 +2,26 @@ import type ResponseListApi from '../interfaces/ResponseListApi.tsx'
 
 export const url = 'https://api.potterdb.com/v1/potions'
 
+const defaultFilter =
+  '?filter[ingredients_not_null]=true' +
+  '&filter[effects_not_null]=true'
+
+
 export async function getQtyPotions () :Promise<ResponseListApi>{
   const res = await fetch(url +
-    `?page[size]=1`)
+    `${defaultFilter}&page[size]=1`)
   return await res.json()
 }
 
 export async function getDailyPotions (index :number) :Promise<ResponseListApi>{
   const res = await fetch(url +
-    `?page[size]=1&page[nnumber]=${index}`)
+    `${defaultFilter}&page[size]=1&page[number]=${index}`)
   return await res.json()
 }
 
 export async function getPotionsForAutocomplete (query: string): Promise<ResponseListApi> {
   const res = await fetch(url +
-    `?filter[name_cont]=${query}&page[size]=10`
+    `${defaultFilter}&filter[name_cont]=${query}&page[size]=10`
   )
   return await res.json()
 }
